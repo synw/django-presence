f5e6d0769ed31df27722a25c81f176c94e7c3524
syn <synwe@yahoo.fr>
2016-08-04 18:32:48 +0000

    UI fix

--- presence/templates/presence/js/utils.js
+++ presence/templates/presence/js/utils.js
@@ -1,19 +1,21 @@
 {% load i18n %}
 
+var debug = false;
+
 function format_users(users, num_anonymous) {
 	var content = '<ul style="list-style:none">';
 	content = content+'<li style="display:inline;color:grey">Online:</li>';
-	//console.log("USERS: "+users);
+	if (debug === true) {console.log("USERS: "+users)};
 	if (users.length > 0) {
 		for (var i = 0; i < users.length; i++) {
 			user = users[i];
-		    //console.log("USER: "+user);
+			if (debug === true) {console.log("USER: "+user)};
 		    content = content+'<li style="display:inline;padding:0 0.5em 0 0.5em"><i class="fa fa-user" style="color:lightgrey"></i> '+user+'</li>';
 		};
 	}
 	var num_anonymous_ = parseFloat(num_anonymous);
 	if ( num_anonymous > 0) {
-		console.log(num_anonymous_);
+		if (debug === true) {console.log(num_anonymous_)};
 		if ( num_anonymous_ == 1) {
 			content = content+'<li style="display:inline;padding:0 0.5em 0 0.5em"> '+num_anonymous+" {% trans 'anonymous user' %}"+'</li>';
 		}
